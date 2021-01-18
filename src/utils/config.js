5075e99bffa6a32df494b440907e4038e732d1eb
Henrik Olsson <henrikolsson@outlook.com>
2021-01-18 11:04:07 +0100

    Change to queue instead of killing process * Move config to util

---
+++ src/utils/config.js
@@ -0,0 +1,62 @@
+const configs = require('../config.json')
+
+function getConfigForStatus (info) {
+  const repository = info.cloneUrl.replace('https://github.com/', '').slice(0, -4) // Remove '.git'
+  const branch = info.branch
+  return getConfigForRepository(repository, branch)
+}
+
+function getConfigForPayload (eventType, payload) {
+  const branch = getBranchFromPayload(eventType, payload)
+  const repository = payload.repository.full_name
+  return getConfigForRepository(repository, branch)
+}
+
+function getConfigForRepository (repository, branch) {
+  const matchingConfigs = []
+  for (const config of configs) {
+    if (repository === config.repository) {
+      if (config.branch !== undefined && config.branch !== branch) {
+        continue
+      }
+
+      if (config.branchBlackList !== undefined && config.branchBlackList.includes(branch)) {
+        continue
+      }
+
+      matchingConfigs.push(config)
+    }
+  }
+
+  if (matchingConfigs.length === 0) {
+    throw new Error('No matching configs for: ' + JSON.stringify({
+      branch,
+      repository
+    }))
+  }
+
+  if (matchingConfigs.length > 1) {
+    console.warn('Multiple matching configs for: ' + JSON.stringify({
+      branch,
+      repository
+    }) + ' selecting first found')
+  }
+
+  return matchingConfigs[0]
+}
+
+function getBranchFromPayload (eventType, payload) {
+  switch (eventType) {
+    case 'pull_request':
+      return payload.pull_request.head.ref
+    case 'check_run':
+      return payload.check_run.check_suite.head_branch
+    case 'delete':
+      return payload.ref.replace('refs/heads/') // This will always return as "refs/heads/BRANCH"
+  }
+}
+
+module.exports = {
+  getConfigForStatus,
+  getConfigForPayload
+}
