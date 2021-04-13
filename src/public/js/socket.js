40c4d0719a285a89515c7db9cc2cc0c3fa05059c
Henrik Olsson <henrikolsson@outlook.com>
2021-04-13 13:57:30 +0200

    Try using WebSocket

--- src/public/js/socket.js
+++ src/public/js/socket.js
@@ -1,15 +1,15 @@
-const evtSource = new EventSource('/status/events?channel=status')
+const hrefNoProtocol = location.href.replace(location.protocol, '')
+const ws = new WebSocket(location.hostname === 'localhost' ? 'ws://localhost:3000' : 'wss://' + hrefNoProtocol)
 const domparser = new DOMParser()
-evtSource.addEventListener('status', function (event) {
-  const html = JSON.parse(event.data)
-  const node = document.getElementById('status')
-  node.innerHTML = ''
-  node.appendChild(domparser.parseFromString(html, 'text/html').body)
-})
+ws.addEventListener('message', function (event) {
+  const message = JSON.parse(event.data)
+  let node
+  if (message.event === 'status') {
+    node = document.getElementById('status')
+  } else if (message.event === 'queue') {
+    node = document.getElementById('queue')
+  }
 
-evtSource.addEventListener('queue', function (event) {
-  const html = JSON.parse(event.data)
-  const node = document.getElementById('queue')
   node.innerHTML = ''
-  node.appendChild(domparser.parseFromString(html, 'text/html').body)
+  node.appendChild(domparser.parseFromString(message.html, 'text/html').body)
 })
